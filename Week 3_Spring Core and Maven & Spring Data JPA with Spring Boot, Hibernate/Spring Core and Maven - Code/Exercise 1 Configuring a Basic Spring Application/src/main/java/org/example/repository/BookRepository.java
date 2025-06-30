d79a138a39944f0765d5136b1e563c4e6490a03c
Rithik Raja S <rithikraja28.rr@gmail.com>
2025-06-30 20:22:31 +0530

    Exercise 1: Configuring a Basic Spring Application - Completed

---
+++ Week 3_Spring Core and Maven & Spring Data JPA with Spring Boot, Hibernate/Spring Core and Maven - Code/Exercise 1 Configuring a Basic Spring Application/src/main/java/org/example/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package org.example.repository;
+
+public class BookRepository {
+    public void saveBook(String bookName)
+    {
+        System.out.println("BookRepository: Saving book - " + bookName);
+    }
+}
