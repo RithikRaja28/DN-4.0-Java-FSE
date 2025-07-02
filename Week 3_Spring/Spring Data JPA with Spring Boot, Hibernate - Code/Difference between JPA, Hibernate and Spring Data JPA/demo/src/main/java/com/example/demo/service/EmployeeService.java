357bb580ebdaff3c77a3908ac32135f7254572d8
Rithik Raja S <rithikraja28.rr@gmail.com>
2025-07-02 21:28:52 +0530

    Week 3

---
+++ Week 3_Spring/Spring Data JPA with Spring Boot, Hibernate - Code/Difference between JPA, Hibernate and Spring Data JPA/demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Transactional
+    public Employee addEmployee(Employee emp)
+    {
+        return employeeRepository.save(emp);
+    }
+
+}
